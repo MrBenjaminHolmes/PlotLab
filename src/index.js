8911a30bf1bf8f6171f11941dee754e977ad35a4
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2025-07-03 22:54:12 +0100

    Evaluating Expressions

--- src/index.js
+++ src/index.js
@@ -1,36 +1,65 @@
 import "./styles.css";
 import { resizeCanvas, setScale } from "./drawGraph.js";
+import * as math from "mathjs";
 
 const canvas = document.getElementById("GraphArea");
 const ctx = canvas.getContext("2d");
 const newExpress = document.querySelector("#newExpressionButton");
 const sidebar = document.getElementById("sidebar");
-
+const BASE_MAJOR_SPACING = 50;
+const BASE_MINOR_SPACING = 10;
 let scale = 1.0;
 
-function updateScale(newScale) {
-  scale = newScale;
-  setScale(scale);
+function update() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  resizeCanvas(canvas, ctx);
+  getAllExpressions().forEach((element) => {
+    evaluateExpression(element);
+  });
 }
 
-canvas.addEventListener("wheel", (event) => {
-  event.preventDefault();
-  if (event.deltaY < 0) updateScale(scale + 0.1);
-  if (event.deltaY > 0) updateScale(Math.max(0.1, scale - 0.1));
-  resizeCanvas(canvas, ctx);
-});
+function evaluateExpression(expr) {
+  try {
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const centerX = width / 2;
+    const centerY = height / 2;
 
-canvas.addEventListener("auxclick", (event) => {
-  if (event.button === 1) {
-    updateScale(1.0);
-    resizeCanvas(canvas, ctx);
+    ctx.beginPath();
+    ctx.strokeStyle = "blue";
+
+    let first = true;
+
+    for (let screenX = 0; screenX <= width; screenX += 1) {
+      // Convert screen X to math X
+      const x = (screenX - centerX) / (scale * BASE_MAJOR_SPACING); // real x
+
+      const y = math.evaluate(expr, { x });
+
+      // Convert math Y to screen Y
+      const screenY = centerY - y * scale * BASE_MAJOR_SPACING;
+
+      if (first) {
+        ctx.moveTo(screenX, screenY);
+        first = false;
+      } else {
+        ctx.lineTo(screenX, screenY);
+      }
+    }
+
+    ctx.stroke();
+  } catch (e) {
+    console.log("Invalid Expression: " + expr + e);
   }
-});
+}
 
-window.addEventListener("resize", () => resizeCanvas(canvas, ctx));
-window.addEventListener("load", () => resizeCanvas(canvas, ctx));
+function getAllExpressions() {
+  const inputs = sidebar.querySelectorAll(".expressionInput input");
+  const values = Array.from(inputs).map((input) => input.value.trim());
+  return values.filter((val) => val !== "");
+}
 
-newExpress.addEventListener("click", () => {
+function newExpressionInput() {
   const div = document.createElement("div");
   const expressionCount =
     sidebar.querySelectorAll(".expressionInput").length + 1;
@@ -52,6 +81,42 @@ newExpress.addEventListener("click", () => {
   removeButton.addEventListener("click", () => {
     div.remove();
   });
+
+  div.firstElementChild.nextElementSibling.addEventListener(
+    "input",
+    (event) => {
+      update();
+      console.log(getAllExpressions());
+    }
+  );
+}
+
+function updateScale(newScale) {
+  scale = newScale;
+  setScale(scale);
+}
+
+newExpressionInput();
+
+canvas.addEventListener("wheel", (event) => {
+  event.preventDefault();
+  if (event.deltaY < 0) updateScale(scale + 0.1);
+  if (event.deltaY > 0) updateScale(Math.max(0.1, scale - 0.1));
+  update();
+});
+
+canvas.addEventListener("auxclick", (event) => {
+  if (event.button === 1) {
+    updateScale(1.0);
+    update();
+  }
+});
+
+window.addEventListener("resize", () => resizeCanvas(canvas, ctx));
+window.addEventListener("load", () => resizeCanvas(canvas, ctx));
+
+newExpress.addEventListener("click", () => {
+  newExpressionInput();
 });
 
 if (module.hot) {
